85722cd8047d0874d234efe678f263d526ec5a81
max razhnov <shahteryshka@gmail.com>
2019-05-11 01:08:37 +0300

    feat: add button, footer, correct styles

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './Footer.css';
+class Footer extends Component {
+  render() {
+    return (
+      <div className="wrapper footer">
+        <span className="footer__title">© 2019 Wargaming.net</span>
+      </div>
+    )
+  }
+}
+
+export default Footer;
