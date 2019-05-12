e541aa624384780a6423731b98be849229a0ab3f
Mudreuski <bahus1994@mail.ru>
2019-05-13 02:22:11 +0300

    Updates

--- precache-manifest.95c1246bcc569cfc55b2c94e2621667a.js
+++ precache-manifest.95c1246bcc569cfc55b2c94e2621667a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "97b8ca42f68a65d8e36efad9f1765c5a",
+    "revision": "afd18e7ae58b7dd344578767497d5981",
     "url": "/share_coffee_frontend/index.html"
   },
   {
-    "revision": "8ffdc1a3790d539ce226",
+    "revision": "9f991c6808ada461492f",
     "url": "/share_coffee_frontend/static/css/main.8947bee6.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/share_coffee_frontend/static/js/2.9a08430f.chunk.js"
   },
   {
-    "revision": "8ffdc1a3790d539ce226",
-    "url": "/share_coffee_frontend/static/js/main.c7eb6704.chunk.js"
+    "revision": "9f991c6808ada461492f",
+    "url": "/share_coffee_frontend/static/js/main.210a4624.chunk.js"
   },
   {
     "revision": "65eed05b43ebe664c167",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/share_coffee_frontend/precache-manifest.0ce96b3a530795005a193ad44ab3a904.js"
+  "/share_coffee_frontend/precache-manifest.95c1246bcc569cfc55b2c94e2621667a.js"
 );
 
 self.addEventListener('message', (event) => {
