f33c95f425d864cbf3e3a98b5f3cb93adf04207c
AlesiaSk <alesya213@gmail.com>
2019-05-17 22:05:57 +0300

    Added jsdoc comments for dropdowns

--- client/src/components/Dropdown/index.jsx
+++ client/src/components/Dropdown/index.jsx
@@ -1,40 +1,62 @@
-import React, {useState} from 'react';
-import styles from './styles.module.scss';
-import List from './List';
-import { ReactComponent as DropdownArrowIcon } from '../../icons/DropdownArrow.svg'
+import React, { useState } from "react";
+import styles from "./styles.module.scss";
+import List from "./List";
+import { ReactComponent as DropdownArrowIcon } from "../../icons/DropdownArrow.svg";
 
 let timeOutId = null;
 
 const getLabelByValue = (options, value) => {
-    const option = (options || []).find(option => option.value === value);
+  const option = (options || []).find(option => option.value === value);
 
-    return option.label;
+  return option.label;
 };
 
-const Dropdown = ({options, selectedValue, onSelect}) => {
-    const [isOpened, setIsOpened] = useState(false);
-    const selection = selectedValue ? getLabelByValue(options, selectedValue) : 'Accounts';
+/**
+ * Atomic dropdown component
+ * @param options (array of label-value objects)
+ * example: [{label: "label1", value: "value1"}, {label: "label2", value: "value2"}]
+ */
+const Dropdown = ({ options, selectedValue, onSelect }) => {
+  const [isOpened, setIsOpened] = useState(false);
+  const selection = selectedValue
+    ? getLabelByValue(options, selectedValue)
+    : "Accounts";
 
-    return (
-        <div
-            onFocus={() => clearTimeout(timeOutId)}
-            onBlur={() => {
-                timeOutId = setTimeout(() => {
-                    setIsOpened(false);
-                });
-            }}
-            className={`${styles.dropdown_container} ${isOpened ? styles['focused'] : undefined}`}
-        >
-            <div tabIndex="0" className={styles.selection}  onClick={() => setIsOpened(!isOpened)}>
-                {selection}
-                <DropdownArrowIcon className={`${styles.arrow} ${isOpened ? styles['rotated'] : undefined}`}/>
-            </div>
-            {isOpened && <List onItemClick={value => {
-                onSelect(value);
-                setIsOpened(false);
-            }} options={options}/>}
-        </div>
-    )
+  return (
+    <div
+      onFocus={() => clearTimeout(timeOutId)}
+      onBlur={() => {
+        timeOutId = setTimeout(() => {
+          setIsOpened(false);
+        });
+      }}
+      className={`${styles.dropdown_container} ${
+        isOpened ? styles["focused"] : undefined
+      }`}
+    >
+      <div
+        tabIndex="0"
+        className={styles.selection}
+        onClick={() => setIsOpened(!isOpened)}
+      >
+        {selection}
+        <DropdownArrowIcon
+          className={`${styles.arrow} ${
+            isOpened ? styles["rotated"] : undefined
+          }`}
+        />
+      </div>
+      {isOpened && (
+        <List
+          onItemClick={value => {
+            onSelect(value);
+            setIsOpened(false);
+          }}
+          options={options}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Dropdown;
--- client/src/components/EventsDropdown/index.jsx
+++ client/src/components/EventsDropdown/index.jsx
@@ -1,30 +1,40 @@
-import React, {useState} from 'react';
-import PropTypes from 'prop-types';
-import styles from './styles.module.scss';
-import List from './List';
-import { ReactComponent as ArrowIcon } from '../../icons/Arrow.svg'
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import styles from "./styles.module.scss";
+import List from "./List";
+import { ReactComponent as ArrowIcon } from "../../icons/Arrow.svg";
 
-const EventsDropdown = ({events}) => {
-    const [isOpened, setIsOpened] = useState(false);
+/**
+ * Dropdown component for showing user events
+ * @param events (array of label-value objects)
+ * example:
+ * [{name: "name1", place: "place1", time:"1st September"}, {name: "name2", place: "place2", time:"1st September"}]
+ */
+const EventsDropdown = ({ events }) => {
+  const [isOpened, setIsOpened] = useState(false);
 
-    return (
-        <div
-            onMouseLeave={() => {
-                setIsOpened(false);
-            }}
-            className={styles.list_container}
-        >
-
-            <button className={styles.header} onClick={() => setIsOpened(!isOpened)}>My upcoming events ({events.length})
-                <ArrowIcon className={`${styles.arrow} ${isOpened ? styles['rotated'] : undefined}`}/>
-            </button>
-            {isOpened && <List events={events}/>}
-        </div>
-    )
+  return (
+    <div
+      onMouseLeave={() => {
+        setIsOpened(false);
+      }}
+      className={styles.list_container}
+    >
+      <button className={styles.header} onClick={() => setIsOpened(!isOpened)}>
+        My upcoming events ({events.length})
+        <ArrowIcon
+          className={`${styles.arrow} ${
+            isOpened ? styles["rotated"] : undefined
+          }`}
+        />
+      </button>
+      {isOpened && <List events={events} />}
+    </div>
+  );
 };
 
 EventsDropdown.propTypes = {
-    events: PropTypes.array
+  events: PropTypes.array,
 };
 
 export default EventsDropdown;
