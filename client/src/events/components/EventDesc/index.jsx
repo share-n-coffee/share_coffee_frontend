a876c9773bba06117fedad62b8937ac8816c8258
max razhnov <shahteryshka@gmail.com>
2019-05-17 21:16:20 +0300

    fix: refactor files and structure of app

--- client/src/events/components/EventDesc/index.jsx
+++ client/src/events/components/EventDesc/index.jsx
@@ -6,32 +6,27 @@ import Button from "../Button";
 import styles from "./styles.module.scss";
 
 class EventDesc extends Component {
-
   render() {
     const { events } = this.props;
-    const elements = events.map((item) => {
-      const {...itemProps} = item;
+    const elements = events.map(item => {
+      const { ...itemProps } = item;
 
       return (
         <div key={itemProps.key} className={styles.eventDescItem}>
           <div>
             <EventName eventName={itemProps.eventName} />
-            <InfoAboutEvent adress={itemProps.adress} eventFrequency={itemProps.eventFrequency} />
+            <InfoAboutEvent
+              adress={itemProps.adress}
+              eventFrequency={itemProps.eventFrequency}
+            />
           </div>
           <Button text={itemProps.subscribe} />
         </div>
-
       );
     });
 
-    return (
-      <div className={styles.eventDesc}>
-        {elements}
-      </div>
-    );
-
+    return <div className={styles.eventDesc}>{elements}</div>;
   }
-  
 }
 
 export default EventDesc;
---
+++ client/src/modules/Button/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const Button = props => {
+  const { text, handlerClick } = props;
+  return (
+    <button className={`${styles.section__btn}`} onClick={handlerClick}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
