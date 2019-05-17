7473061e62f99c3c2af431c3e0268756c8c6144c
max razhnov <shahteryshka@gmail.com>
2019-05-17 21:17:48 +0300

    feat: add dir for helpers func

---
+++ client/src/helpers/TelegramLoginButton/index.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+// import PropTypes from "prop-types";
+import styles from "./styles.module.scss";
+
+export default class TelegramLoginButton extends Component {
+  componentDidMount() {
+    const {
+      botName,
+      buttonSize,
+      cornerRadius,
+      requestAccess,
+      usePic,
+      dataOnauth,
+    } = this.props;
+    window.TelegramLoginWidget = {
+      dataOnauth: user => {
+        dataOnauth(user);
+      },
+    };
+
+    const script = document.createElement("script");
+    script.src = "https://telegram.org/js/telegram-widget.js?4";
+    script.setAttribute("data-telegram-login", botName);
+    script.setAttribute("data-size", buttonSize);
+    if (cornerRadius !== undefined) {
+      script.setAttribute("data-radius", cornerRadius);
+    }
+    script.setAttribute("data-request-access", requestAccess);
+    script.setAttribute("data-userpic", usePic);
+    script.setAttribute("data-onauth", "TelegramLoginWidget.dataOnauth(user)");
+    script.async = true;
+    this.instance.appendChild(script);
+  }
+
+  render() {
+    return (
+      <div
+        className={styles.telegram__iframe__block}
+        ref={component => {
+          // console.log("this.props.className: " + this.props.className);
+          // console.dir(component);
+          // console.dir(this.instance);
+          this.instance = component;
+        }}
+      />
+    );
+  }
+}
+
+// TelegramLoginButton.propTypes = {
+//   botName: PropTypes.string.isRequired,
+//   dataOnauth: PropTypes.func,
+//   buttonSize: PropTypes.oneOf(["large", "medium", "small"]),
+//   cornerRadius: PropTypes.number,
+//   requestAccess: PropTypes.string,
+//   usePic: PropTypes.bool,
+// };
+
+// TelegramLoginButton.defaultProps = {
+//   botName: "samplebot",
+//   dataOnauth: () => undefined,
+//   buttonSize: "large",
+//   requestAccess: "write",
+//   usePic: true,
+// };
