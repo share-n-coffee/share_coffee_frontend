e3ed7e2f724dbb8517a99fcf7c318e4c0e1d55b5
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2019-05-18 19:04:50 +0300

    chore: add a story for TopicDescription

--- client/stories/index.stories.js
+++ client/stories/index.stories.js
@@ -1,19 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from "@storybook/react";
 
-import { Button, Welcome } from '@storybook/react/demo';
+import TopicDescription from "../src/TopicDescription";
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
+// Topic description
+const onChangeCallback = renderedHtml => console.log(renderedHtml);
+const data = `<h3 class="md-block-header-three">Hello! This is header!</h3><p class="md-block-unstyled">This is a <strong class="md-inline-bold">bold</strong> text.</p><p class="md-block-unstyled">This is a <span class="md-inline-highlight">highlighted</span> text.</p><p class="md-block-unstyled">This is a <u class="md-inline-underline">underline</u> text and <a class="md-inline-link" href="http://wg.net" target="_blank" rel="noopener noreferrer">link</a>.</p>`;
+storiesOf("Topic description", module)
+  .add("read-only with text", () => <TopicDescription data={data} />)
+  .add("read-only without text", () => <TopicDescription />)
+  .add("editable with text", () => (
+    <TopicDescription data={data} editable={true} onChange={onChangeCallback} />
+  ))
+  .add("editable without text", () => (
+    <TopicDescription editable={true} onChange={onChangeCallback} />
   ));
