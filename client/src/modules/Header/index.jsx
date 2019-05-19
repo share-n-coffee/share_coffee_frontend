6b2c667cc8874a0297842c894b219921db5e5398
max razhnov <shahteryshka@gmail.com>
2019-05-19 12:20:36 +0300

    feat: add navigation and condition for pages

--- client/src/modules/Header/index.jsx
+++ client/src/modules/Header/index.jsx
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
 import logo from "./logo-coffee.png";
 import styles from "./styles.module.scss";
-
+import Button from "../Button";
+import Dropdown from "../../components/Dropdown";
 class Header extends Component {
   render() {
+    //check if user login
+    const isActive = true;
+    //
+    const avatar = "https://t.me/i/userpic/320/MxmMazovsky.jpg";
+    const name = "FullStack921";
+    const navBlock = () => {
+      return (
+        <div>
+          <div className={styles.nav}>
+            <img className={styles.user__img} src={avatar} alt="avatar" />
+            <span className={styles.user__info}>{name}</span>
+            <Button text={"Log out "} className={styles.btn_logout_style} />
+          </div>
+          <div className={styles.header__dropdown}>
+            <Dropdown />
+          </div>
+        </div>
+      );
+    };
+
     return (
       <div className={`${styles.wrapper} ${styles.header}`}>
-        <img src={logo} className={styles.logo_header} alt="coffee" />
+        <div className={styles.header__container}>
+          <img src={logo} className={styles.logo_header} alt="coffee" />
+          {isActive ? navBlock() : <div />}
+        </div>
       </div>
     );
   }
