dced96b582a82e757b0a21dbcc091084b2593d1a
Loor <loor@list.ru>
2019-05-20 19:00:02 +0300

    feat:add route to full info

--- client/src/events/components/EventDesc/index.jsx
+++ client/src/events/components/EventDesc/index.jsx
@@ -1,19 +1,35 @@
 import React, { Component } from "react";
 import InfoAboutEvent from "../../modules/InfoAboutEvent";
+import TopicFront from "../../../pages/TopicFront";
 import EventName from "../../modules/EventName";
 import Button from "../../../common/Button";
 
 import styles from "./styles.module.scss";
 
 class EventDesc extends Component {
+  state = {
+    idEvent: "",
+    openEvent: false,
+  };
+  openEvent(id) {
+    return async () => {
+      await this.setState({ idEvent: id });
+      await this.setState({ openEvent: true });
+      this.forceUpdate();
+      console.log(this.state);
+    };
+  }
+
   render() {
     const { events } = this.props;
     const elements = events.map(item => {
       const { ...itemProps } = item;
-
       return (
-        <div key={itemProps.key} className={styles.eventDescItem}>
-          <div className={styles.eventContainer}>
+        <div key={itemProps._id} className={styles.eventDescItem}>
+          <div
+            className={styles.eventContainer}
+            onClick={this.openEvent(itemProps._id)}
+          >
             <EventName
               eventName={itemProps.title}
               isSubscribed={itemProps.isActive}
@@ -31,7 +47,11 @@ class EventDesc extends Component {
       );
     });
 
-    return <div className={styles.eventDesc}>{elements}</div>;
+    return this.state.openEvent ? (
+      <TopicFront id={this.state.idEvent} />
+    ) : (
+      <div className={styles.eventDesc}>{elements}</div>
+    );
   }
 }
 
