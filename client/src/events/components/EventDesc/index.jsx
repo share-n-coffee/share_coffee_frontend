df143fe803bf27a6ae540d800b2c735a87b7fc05
Loor <loor@list.ru>
2019-05-21 04:00:28 +0300

    feat: add change PageTitle to ←Back

--- client/src/events/components/EventDesc/index.jsx
+++ client/src/events/components/EventDesc/index.jsx
@@ -1,24 +1,39 @@
 import React, { Component } from "react";
 import InfoAboutEvent from "../../modules/InfoAboutEvent";
+import PageTitle from "../../../modules/PageTitle";
 import TopicFront from "../../../pages/TopicFront";
 import EventName from "../../modules/EventName";
 import Button from "../../../common/Button";
-
 import styles from "./styles.module.scss";
 
 class EventDesc extends Component {
   state = {
     idEvent: "",
     openEvent: false,
+    linkNoHover: true,
   };
-  openEvent(id) {
-    return async () => {
-      await this.setState({ idEvent: id });
-      await this.setState({ openEvent: true });
+  openEvent(id, title) {
+    return () => {
+      this.setState({ idEvent: id });
+      this.setState({ title: title });
+      this.setState({ openEvent: true });
+
+      this.setState({ linkNoHover: true });
       this.forceUpdate();
-      console.log(this.state);
     };
   }
+
+  mouseEvents = {
+    mouseOver: () => {
+      this.setState({ linkNoHover: false });
+    },
+    mouseOut: () => {
+      this.setState({ linkNoHover: true });
+    },
+    click: () => {
+      this.setState({ openEvent: false });
+    },
+  };
 
   render() {
     const { events } = this.props;
@@ -28,7 +43,7 @@ class EventDesc extends Component {
         <div key={itemProps._id} className={styles.eventDescItem}>
           <div
             className={styles.eventContainer}
-            onClick={this.openEvent(itemProps._id)}
+            onClick={this.openEvent(itemProps._id, itemProps.title)}
           >
             <EventName
               eventName={itemProps.title}
@@ -46,11 +61,22 @@ class EventDesc extends Component {
         </div>
       );
     });
-
     return this.state.openEvent ? (
-      <TopicFront id={this.state.idEvent} />
+      <>
+        <PageTitle
+          title={this.state.linkNoHover ? this.state.title : "← Back"}
+          mouseOver={this.mouseEvents.mouseOver}
+          mouseOut={this.mouseEvents.mouseOut}
+          click={this.mouseEvents.click}
+        />
+
+        <TopicFront id={this.state.idEvent} />
+      </>
     ) : (
-      <div className={styles.eventDesc}>{elements}</div>
+      <>
+        <PageTitle title="Current topics" />
+        <div className={styles.eventDesc}>{elements}</div>
+      </>
     );
   }
 }
--- client/src/pages/SubscriptionsPage/index.jsx
+++ client/src/pages/SubscriptionsPage/index.jsx
@@ -1,11 +1,8 @@
 import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import styles from "./styles.module.scss";
-import PageTitle from "../../modules/PageTitle";
 import EventDesc from "../../Events/components/EventDesc";
 import UserDataContext from "../../contexts/UserDataContext";
-import Header from "../../common/Header";
-import Footer from "../../common/Footer";
 
 const getEvents = token => {
   return axios({
@@ -31,7 +28,6 @@ const SubscriptionsPage = () => {
 
   return (
     <main>
-      <PageTitle title="Current topics" />
       <EventDesc className={styles.event} events={events} />
     </main>
   );
