29f522601ca8d5d3fc39308ef08e622cba97e007
Loori-R <Loor@list.ru>
2019-05-22 04:09:42 +0300

    refact: transfer functions from components

---
+++ client/src/pages/LoginPage/helpers.js
@@ -0,0 +1,24 @@
+const setStorage = (token, userData) => {
+  sessionStorage.setItem("token", token);
+  sessionStorage.setItem("id", userData.data._id);
+  sessionStorage.setItem("firstName", userData.data.firstName);
+  sessionStorage.setItem("lastName", userData.data.lastName);
+  sessionStorage.setItem("avatar", userData.data.avatar);
+  sessionStorage.setItem("isAdmin", userData.data.isAdmin);
+  sessionStorage.setItem("banned", userData.data.banned.status);
+  sessionStorage.setItem("department", userData.data.department);
+  sessionStorage.setItem("tokenTimeOver", userData.exp);
+  sessionStorage.setItem("tokenTimeStart", userData.iat);
+};
+
+const router = props => {
+  const id = sessionStorage.getItem("id");
+  const departament = sessionStorage.getItem("department");
+  if (id && !departament) {
+    props.history.push("/team_select/");
+  } else if (id && departament) {
+    props.history.push("/subscriptions/");
+  }
+};
+
+export { setStorage, router };
