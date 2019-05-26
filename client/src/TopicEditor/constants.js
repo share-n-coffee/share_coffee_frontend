97a9e77829d8ddff1d578ddc638a1e50a37fcafe
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2019-05-26 18:08:05 +0300

    refactor: update TopicEditor constants

--- client/src/TopicEditor/constants.js
+++ client/src/TopicEditor/constants.js
@@ -1,17 +1,6 @@
-const PREDEFINED_TOPIC = {
-  title: "",
-  description: "",
-  weekDay: "",
-  date: "",
-  time: "",
-  place: "",
-  isRegular: null,
-  location: [],
-};
-
 const PLACEHOLDERS = {
   location: "Coordinates",
-  place: "Place",
+  address: "Place",
   time: "Time",
   title: "Topic Title",
 };
@@ -19,15 +8,17 @@ const PLACEHOLDERS = {
 const DEFAULT_COORDINATES = [0, 0];
 const COORDINATES_SEP = ", ";
 
-const REGULARITY = {
-  periodic: "true",
-  single: "false",
-};
+const CYCLIC = [
+  {
+    name: "periodic",
+    value: true,
+    label: "Periodic",
+  },
+  {
+    name: "single",
+    value: false,
+    label: "Single",
+  },
+];
 
-export {
-  PREDEFINED_TOPIC,
-  PLACEHOLDERS,
-  COORDINATES_SEP,
-  DEFAULT_COORDINATES,
-  REGULARITY,
-};
+export { PLACEHOLDERS, COORDINATES_SEP, DEFAULT_COORDINATES, CYCLIC };
