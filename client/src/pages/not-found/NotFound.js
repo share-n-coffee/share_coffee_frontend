4aff8a3692434a6322601830951eb68f08746925
max razhnov <shahteryshka@gmail.com>
2019-05-26 18:17:34 +0300

    fix: change 404 page

--- client/src/pages/not-found/NotFound.js
+++ client/src/pages/not-found/NotFound.js
@@ -1,16 +1,33 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import john from "../../assets/img/john.gif";
+import Header from "../../common/Header";
+import Button from "../../common/Button";
 
 class NotFound extends Component {
   render() {
     return (
-      <div className="login_container">
-        <h1>404</h1>
-        <h2>Page not found</h2>
-        <Link to="/" className="btn">
-          Home
-        </Link>
-      </div>
+      <>
+        <Header />
+        <div className="notfound_container">
+          <div className="login_container">
+            <h1>404</h1>
+            <h2>Page not found</h2>
+            <div className="btn_container">
+              <Button
+                text={"Home"}
+                onClick={() => {
+                  window.location.replace("/");
+                }}
+              />
+            </div>
+            <div className="gif__container">
+              <span className="redux_txt">Redux</span>
+              <span className="tests_txt">Tests</span>
+              <img src={john} className="gif" alt="john" />
+            </div>
+          </div>
+        </div>
+      </>
     );
   }
 }
