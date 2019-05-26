4cbe24d9faea0feb7d38e95b04b44c9710651db9
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2019-05-26 14:11:16 +0300

    refactor: DatePicker

--- client/src/TopicEditor/DatePicker/index.jsx
+++ client/src/TopicEditor/DatePicker/index.jsx
@@ -1,21 +1,23 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import Calendar from "react-calendar/dist/entry.nostyle";
 
 import DoubleArrowIcon from "./DoubleArrowIcon";
 import ArrowIcon from "./ArrowIcon";
+import locale from "./constants";
 
 import "./styles.scss";
 
-function DatePicker({ date, onChange }) {
+function DatePicker({ activeStartDate, onChange }) {
   const next2Label = <DoubleArrowIcon direction={"right"} />;
   const nextLabel = <ArrowIcon direction={"right"} />;
   const prev2Label = <DoubleArrowIcon direction={"left"} />;
   const prevLabel = <ArrowIcon direction={"left"} />;
   return (
     <Calendar
+      activeStartDate={activeStartDate}
       onChange={onChange}
-      locale={"en-US"}
-      value={date}
+      locale={locale}
       nextLabel={nextLabel}
       next2Label={next2Label}
       prevLabel={prevLabel}
@@ -23,5 +25,10 @@ function DatePicker({ date, onChange }) {
     />
   );
 }
+
+DatePicker.propTypes = {
+  activeStartDate: PropTypes.instanceOf(Date),
+  onChange: PropTypes.func,
+};
 
 export default DatePicker;
