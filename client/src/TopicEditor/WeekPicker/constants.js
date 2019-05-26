38f5d759e93621da31b9222bf4baf56154dde155
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2019-05-26 13:56:46 +0300

    refactor: change type of week days object’s values

--- client/src/TopicEditor/WeekPicker/constants.js
+++ client/src/TopicEditor/WeekPicker/constants.js
@@ -1,11 +1,11 @@
 const WEEK_DAYS = {
-  monday: "0",
-  tuesday: "1",
-  wednesday: "2",
-  thursday: "3",
-  friday: "4",
-  saturday: "5",
-  sunday: "6",
+  monday: 1,
+  tuesday: 2,
+  wednesday: 3,
+  thursday: 4,
+  friday: 5,
+  saturday: 6,
+  sunday: 0,
 };
 
 const WEEK_DAYS_NAMES = {
