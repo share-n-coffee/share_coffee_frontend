f2a1a42fd159f763f4baf3db3d6ab4a68c3a65f9
Mudreuski <bahus1994@mail.ru>
2019-05-27 02:09:03 +0300

    style: add someone mobile style

--- client/src/pages/HomeAdmin/Users/userInfo.js
+++ client/src/pages/HomeAdmin/Users/userInfo.js
@@ -4,7 +4,7 @@ class Topics extends Component {
   render() {
     const { user } = this.props;
     return (
-      <div key={user._id} className={""}>
+      <div key={user._id} className="user-info__container">
         <img src={`${user.avatar}`} alt="user photo" />
         <h2>{user.username} </h2>
         <h3>{user.firstName + " " + user.lastName}</h3>
--- client/src/pages/HomeAdmin/Users/userTopics.js
+++ client/src/pages/HomeAdmin/Users/userTopics.js
@@ -55,7 +55,7 @@ class UserTopics extends Component {
   render() {
     const { events, error } = this.props;
     return (
-      <div>
+      <div className="user-topic__container">
         {events &&
           events.length > 0 &&
           events.map(event => <OneTopics id={event.eventId} key={event.eventId} />)}
