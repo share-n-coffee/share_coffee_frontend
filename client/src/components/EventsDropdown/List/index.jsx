28740c4fd5ff167089e347a0020f99482783b614
max razhnov <shahteryshka@gmail.com>
2019-05-29 20:09:10 +0300

    fix: correct dropdown

--- client/src/components/EventsDropdown/List/index.jsx
+++ client/src/components/EventsDropdown/List/index.jsx
@@ -1,18 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {
+  timeConverter,
+  letterTransform,
+  checkerNone,
+  secConverter,
+} from "../../../helpers/helpers";
 
 const List = ({ events }) => {
   return (
-    <ul className="event-List">
-      {events.map(({ title, name, place, time }) => (
-        <div className="event-item" key={title}>
-          <h4 className="event_title">{title}</h4>
-          <p className="event_place__name">{name}</p>
-          <p className="event_place">{place}</p>
-          <p className="event_time">{time}</p>
-        </div>
-      ))}
-    </ul>
+    <>
+      {events.length === 0 ? (
+        <ul className="event-List">
+          <div className="event-item">
+            <h4 className="event_title">No confirmed events</h4>
+          </div>
+        </ul>
+      ) : (
+        <ul className="event-List">
+          {events.map(({ id, topic, date }) => (
+            <div className="event-item" key={id}>
+              <h4 className="event_title">{letterTransform(checkerNone(topic.title))}</h4>
+              <p className="event_place">{checkerNone(topic.address)}</p>
+              <p className="event_time">{`${timeConverter(checkerNone(date))} - ${secConverter(
+                date,
+              )}`}</p>
+            </div>
+          ))}
+        </ul>
+      )}
+    </>
   );
 };
 
