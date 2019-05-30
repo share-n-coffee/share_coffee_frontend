bbacb2ad7b6e236af0b2c2af9fd894645c835cf8
Keetmine <Keetmine@gmail.com>
2019-05-30 23:01:39 +0300

    fix user->topics->user

--- client/src/pages/HomeAdmin/Users/oneUser.js
+++ client/src/pages/HomeAdmin/Users/oneUser.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Tab, TabContainer } from "../../../ui/core/home";
 import UserTopics from "./userTopics";
 import UserInfo from "./userInfo";
-import UserLogs from "./userLogs";
 import { request } from "../../../helpers/requests";
 import PageTitle from "../../../modules/PageTitle";
 import * as URL from "../../../constants";
 import Header from "../../../common/Header";
+import md5 from "js-md5";
 
 class OneUser extends React.Component {
   state = {
@@ -31,6 +31,37 @@ class OneUser extends React.Component {
 
   openTab = tabName => {
     this.setState({ activeTab: tabName });
+  };
+
+  toggleBan = user => {
+    const status = {
+      ban: {
+        status: !user.banned.status,
+      },
+    };
+
+    request.put(URL.BAN_USER(user._id), status).then(data => {
+      this.setState({
+        user: data.object.data,
+      });
+      this.setState({ error: data.message });
+    });
+  };
+
+  toggleAdmin = user => {
+    const status = {
+      admin: {
+        permission: user.admin.permission === 1 ? 0 : 1,
+        password: user.admin.permission === 1 ? "" : md5("test"),
+      },
+    };
+
+    request.put(URL.ONE_USER(user._id), status).then(data => {
+      this.setState({
+        user: data.object.data,
+        error: data.message,
+      });
+    });
   };
 
   mouseEvents = {
@@ -74,7 +105,14 @@ class OneUser extends React.Component {
           {/*Logs*/}
           {/*</Tab>*/}
         </TabContainer>
-        {activeTab === "UserInfo" && <UserInfo user={user} error={error} />}
+        {activeTab === "UserInfo" && (
+          <UserInfo
+            user={user}
+            error={error}
+            toggleBan={user => this.toggleBan(user)}
+            toggleAdmin={user => this.toggleAdmin(user)}
+          />
+        )}
         {activeTab === "UserTopics" && <UserTopics events={user.events} error={error} />}
         {/*{activeTab === "UserLogs" && <UserLogs log={user.logs} error={error} />}*/}
       </div>
