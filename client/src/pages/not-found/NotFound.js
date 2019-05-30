a0f7613ecc3eaf736bbadb0e0b4802e464f95833
max razhnov <shahteryshka@gmail.com>
2019-05-31 01:14:43 +0300

    fix: correct 404

--- client/src/pages/not-found/NotFound.js
+++ client/src/pages/not-found/NotFound.js
@@ -42,7 +42,9 @@ class NotFound extends Component {
             </div>
             <div className="gif__container">
               <span className="redux_txt">Redux</span>
+              <span className="redux_txt api">API 3.0</span>
               <span className="tests_txt">Tests</span>
+              <span className="tests_txt deadline">Deadlines</span>
               <img src={john} className="gif" alt="john" />
             </div>
           </div>
