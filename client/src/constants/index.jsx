6568d8f32cd1ec5a94fbea3fbb2c0ca492c16766
Keetmine <Keetmine@gmail.com>
2019-05-30 18:54:04 +0300

    pagination in client page

--- client/src/constants/index.jsx
+++ client/src/constants/index.jsx
@@ -35,7 +35,9 @@ const GET_EVENTS = "https://forge-development.herokuapp.com/api/events/";
 const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjVjZWRhMjQxOTE5M2M5YmI1NDcwM2ZlMCIsImZpcnN0TmFtZSI6IkFsZXN5YSIsImxhc3ROYW1lIjoiU2tvcm9raG9kIiwiZGVwYXJ0bWVudCI6eyJfaWQiOiI1Y2Q2ZjZjMzgxMzcxZDI5N2FjYjJmZDEiLCJ0aXRsZSI6IktvZmZlZSIsImRlc2NyaXB0aW9uIjoiQ2lsbHVtIHVsbGFtY28gYWQgZWxpdCBkdWlzIHBhcmlhdHVyIHNpbnQgdXQgbGFib3JlIHByb2lkZW50IGVsaXQuIEVzdCBleGNlcHRldXIgZXhlcmNpdGF0aW9uIGV0IG9mZmljaWEgYWRpcGlzaWNpbmcgc2l0IGV4ZXJjaXRhdGlvbiBlYSBkZXNlcnVudCBzaW50IGNvbnNlcXVhdC4gRWxpdCBhbGlxdWEgZXN0IGlydXJlIG5pc2kgaWQgYWRpcGlzaWNpbmcgbmlzaSByZXByZWhlbmRlcml0IGxhYm9ydW0gaW4gc2ludCBpcHN1bSBlYS4gTWFnbmEgZG9sb3JlIHZlbmlhbSB2ZW5pYW0gZG9sb3IgaXJ1cmUgZWxpdCBMb3JlbSBwcm9pZGVudC4gT2ZmaWNpYSB2ZWxpdCBlbmltIHV0IGlkIGFkaXBpc2ljaW5nIGN1cGlkYXRhdCBkb2xvcmUuIER1aXMgY29tbW9kbyByZXByZWhlbmRlcml0IGNpbGx1bSBsYWJvcnVtIG1pbmltIGNvbnNlcXVhdCB2ZWxpdCBuaXNpIGFtZXQgcGFyaWF0dXIuIn0sImF2YXRhciI6Imh0dHBzOi8vdC5tZS9pL3VzZXJwaWMvMzIwL3B1aGNoYS5qcGciLCJiYW5uZWQiOnsic3RhdHVzIjpmYWxzZSwiZXhwaXJlZCI6MH0sInBlcm1pc3Npb24iOjB9LCJpYXQiOjE1NTkwNzgxNTQsImV4cCI6MTU1OTY4Mjk1NH0.Al2mZI_s3qlCNQhTGqyZDFGlmkAt6o2qL9qMmvhVvEo`;
 //new api 2.0
 // const GET_EVENTS = `https://forgeserver.herokuapp.com/api/events/`;
-const GET_ALL_TOPICS = `https://forgeserver.herokuapp.com/api/topics/`;
+// const GET_ALL_TOPICS = `https://forgeserver.herokuapp.com/api/topics/`;
+const GET_ALL_TOPICS = (page = 0, limit = 5) =>
+  `https://forgeserver.herokuapp.com/api/topics/?page=${page}&limit=${limit}`;
 const SET_USER_DEPARTMENT = userId => `https://forgeserver.herokuapp.com/api/users/${userId}`;
 const GET_ALL_DEPARTMENTS = `https://forgeserver.herokuapp.com/api/departments/`;
 const GET_USER = id => `https://forgeserver.herokuapp.com/api/users/${id}`;
